6133007da7b6c45d45b3f222f79008fea37bc93f
kapade-lokesh <lkkapade@gmail.com>
2024-09-22 14:49:23 +0530

    configured dot env

--- index.js
+++ index.js
@@ -3,8 +3,13 @@ import { userRouter } from "./src/Routes/user.Routes.js";
 import bodyParser from "body-parser";
 import cors from "cors";
 import { connecDB } from "./src/DB/connectDB.js";
+import dotenv from "dotenv"
 
 const app = express();
+
+const port = process.env.PORT || 5000
+
+dotenv.config({path:"/.env"})
 
 connecDB();
 
@@ -12,6 +17,6 @@ app.use(cors());
 app.use(bodyParser.json());
 app.use(userRouter);
 
-app.listen(8080,()=>{
+app.listen(port,()=>{
     console.log("server started at 8080");
 })
--- src/DB/connectDB.js
+++ src/DB/connectDB.js
@@ -1,7 +1,7 @@
 import mongoose from "mongoose";
 
 const connecDB=async()=>{
-    const connectioninstance = await mongoose.connect("mongodb://localhost:27017/Index-DB");
+    const connectioninstance = await mongoose.connect(`${process.env.MONGO_URI}/${process.env.DB_NAME}`);
     console.log(connectioninstance.connection.name);
 }
 export {connecDB}
